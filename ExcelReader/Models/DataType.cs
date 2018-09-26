486f60d33b2a374a09029312813cba069f372318
ll9 <llwierer@web.de>
2018-09-26 22:07:11 +0200

    added typeguesser and DataType Class

---
+++ ExcelReader/Models/DataType.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelReader.Models
+{
+    public enum DataType
+    {
+        System_Boolean = 0,
+        System_Int32 = 1,
+        System_Int64 = 2,
+        System_Double = 3,
+        System_DateTime = 4,
+        System_String = 5
+    }
+
+    public static class DataTypeExtension
+    {
+        public static string GetSqlDataType(this DataType dataType)
+        {
+            switch (dataType)
+            {
+                case (DataType.System_Boolean):
+                    return "INTEGER";
+                case (DataType.System_DateTime):
+                    return "TEXT";
+                case (DataType.System_Double):
+                    return "DOUBLE";
+                case (DataType.System_Int32):
+                    return "int32";
+                case (DataType.System_Int64):
+                    return "int64";
+                case (DataType.System_String):
+                    return "VARCHAR(60)";
+                default:
+                    return "VARCHAR(60)";
+            }
+        }
+
+        public static dynamic GetDynamicValue(this DataType dataType, string value)
+        {
+            switch (dataType)
+            {
+                case (DataType.System_Boolean):
+                    return bool.Parse(value);
+                case (DataType.System_DateTime):
+                    return DateTime.Parse(value);
+                case (DataType.System_Double):
+                    return double.Parse(value);
+                case (DataType.System_Int32):
+                    return int.Parse(value);
+                case (DataType.System_Int64):
+                    return long.Parse(value);
+                case (DataType.System_String):
+                    return value;
+                default:
+                    return value;
+            }
+        }
+    }
+}
---
+++ ExcelReader/Models/TypeGuesser.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelReader.Models
+{
+    class TypeGuesser
+    {
+        public static DataType GuessType(DataTable dataTable, string columnName)
+        {
+            foreach (var type in Enum.GetValues(typeof(DataType)).Cast<DataType>())
+            {
+                int i;
+                for (i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    //var value = values[i];
+                    var value = dataTable.Rows[i][columnName].ToString();
+                    if (GuessType(value) != type)
+                    {
+                        break;
+                    }
+                }
+                if (i == dataTable.Rows.Count)
+                    return type;
+            }
+            return DataType.System_String;
+        }
+
+        private static DataType GuessType(string str)
+        {
+
+            bool boolValue;
+            Int32 intValue;
+            Int64 bigintValue;
+            double doubleValue;
+            DateTime dateValue;
+
+            // Place checks higher in if-else statement to give higher priority to type.
+
+            if (bool.TryParse(str, out boolValue))
+                return DataType.System_Boolean;
+            else if (Int32.TryParse(str, out intValue))
+                return DataType.System_Int32;
+            else if (Int64.TryParse(str, out bigintValue))
+                return DataType.System_Int64;
+            else if (double.TryParse(str, out doubleValue))
+                return DataType.System_Double;
+            else if (DateTime.TryParse(str, out dateValue))
+                return DataType.System_DateTime;
+            else return DataType.System_String;
+
+        }
+    }
+}
